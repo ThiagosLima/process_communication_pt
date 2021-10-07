34b1d2221be076ea8ac1e61349ebec19aeed5c47
Thiago Lima <thiagolima.unb@gmail.com>
2021-10-07 19:53:49 -0300

    chore: create mix project

---
+++ lib/process_communication_pt.ex
@@ -0,0 +1,18 @@
+defmodule ProcessCommunicationPt do
+  @moduledoc """
+  Documentation for `ProcessCommunicationPt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProcessCommunicationPt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/process_communication_pt_test.exs
@@ -0,0 +1,8 @@
+defmodule ProcessCommunicationPtTest do
+  use ExUnit.Case
+  doctest ProcessCommunicationPt
+
+  test "greets the world" do
+    assert ProcessCommunicationPt.hello() == :world
+  end
+end
