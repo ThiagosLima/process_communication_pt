83cd5ad3fcfee7647ea2dc6818f2a7fe00f8b58f
Thiago Lima <thiagolima.unb@gmail.com>
2021-10-07 20:00:04 -0300

    feat: add spawn cat

---
+++ lib/1_criar_gato/gato.exs
@@ -0,0 +1,8 @@
+defmodule Gato do
+	def criar do
+		IO.puts("Você criou um gato")
+	end
+end
+
+# Cria um gato
+spawn(Gato, :criar, [])
--- test/process_communication_pt_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ProcessCommunicationPtTest do
-  use ExUnit.Case
-  doctest ProcessCommunicationPt
-
-  test "greets the world" do
-    assert ProcessCommunicationPt.hello() == :world
-  end
-end
